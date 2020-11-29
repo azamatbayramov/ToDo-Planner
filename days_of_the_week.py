b9f84c14c1418d70cb22ee76ff8613ae4f14a691
Azamat Bayramov <bayramov.azamat04@gmail.com>
2020-11-29 13:21:40 +0500

    Refactor code
    Refactor code in "days_of_the_week.py" module

--- days_of_the_week.py
+++ days_of_the_week.py
@@ -4,30 +4,42 @@ from all_json import DAYS_OF_THE_WEEK
 # This is a module for working with days of the week
 
 
-# Function for getting days of the week from a string
 def get_days_of_the_week_from_string(input_string, language):
+    """Function for getting days of the week from a string"""
+
     input_string = input_string.lower()
     output_list = []
 
     days_of_the_week = input_string.split()
 
+    # Check if days of the week more than in the week
     if len(days_of_the_week) > 7:
         return False
 
+    # Receive different formats of days of the week
+    days_of_the_week_int = ['1', '2', '3', '4', '5', '6', '7']
+    days_of_the_week_full = DAYS_OF_THE_WEEK["full"][language]
+    days_of_the_week_short = DAYS_OF_THE_WEEK["short"][language]
+
     # Check if all days of the week in integer format
-    if all(map(lambda s: s in ['1', '2', '3', '4', '5', '6', '7'], days_of_the_week)):
+    if all(map(lambda s: s in days_of_the_week_int, days_of_the_week)):
         output_list = list(map(int, days_of_the_week))
 
     # Check if all days of the week in "full-days" format
-    elif all(map(lambda s: s in DAYS_OF_THE_WEEK["full"][language], days_of_the_week)):
+    elif all(map(lambda s: s in days_of_the_week_full, days_of_the_week)):
         for day_of_the_week in days_of_the_week:
-            output_list.append(DAYS_OF_THE_WEEK["full"][language].index(day_of_the_week))
+            output_list.append(
+                days_of_the_week_full.index(day_of_the_week)
+            )
 
     # Check if all days of the week in "short-days" format
-    elif all(map(lambda s: s in DAYS_OF_THE_WEEK["short"][language], days_of_the_week)):
+    elif all(map(lambda s: s in days_of_the_week_short, days_of_the_week)):
         for day_of_the_week in days_of_the_week:
-            output_list.append(DAYS_OF_THE_WEEK["short"][language].index(day_of_the_week))
+            output_list.append(
+                days_of_the_week_short.index(day_of_the_week)
+            )
 
+    # Check if the days of the week are repeated
     if output_list != list(set(output_list)):
         return False
 
