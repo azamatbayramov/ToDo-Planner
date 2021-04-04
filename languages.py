65d387e20091c0a0acf3f5e8dc3dcc61283db62e
Azamat Bayramov <bayramov.azamat04@gmail.com>
2021-04-04 20:49:01 +0500

    Write comments in "languages.py" module

--- languages.py
+++ languages.py
@@ -1,17 +1,25 @@
+"""Module for working with user's languages"""
+
 from data.models import User
 from data import db_session
 from all_json import LANGUAGES
 
 
+# Function for getting user language
 def get_user_language(user_id=None, update=None, short=False):
+    # If passed update, not user_id
     if update:
         user_id = update.message.from_user.id
 
+    # Create database session
     session = db_session.create_session()
 
+    # Get user information
     user = session.query(User).filter(User.telegram_id == user_id).first()
 
+    # If user doesn't exist
     if not user:
+        # Create new object for user with standard parameters
         new_user = User()
         new_user.telegram_id = user_id
         new_user.language_id = 0
@@ -19,12 +27,16 @@ def get_user_language(user_id=None, update=None, short=False):
         session.commit()
         user = new_user
 
+    # Get user language
     language_id = user.language_id
     language = LANGUAGES[language_id]
 
+    # Close database session
     session.close()
 
+    # If passed "short"=True parameter, return short language
     if short:
         return language["short"]
 
+    # Return language
     return language
